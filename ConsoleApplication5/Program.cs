d5977fb35e300a11e1f9a3e599d52b5bb5df6efe
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-05-18 18:48:28 +0800

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,91 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.Content;
+using PdfSharp.Pdf.Content.Objects;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    public static class PdfSharpExtensions
+    {
+        public static IEnumerable<string> ExtractText(this PdfPage page)
+        {
+            var content = ContentReader.ReadContent(page);
+            var text = content.ExtractText();
+            return text;
+        }
+
+        public static IEnumerable<string> ExtractText(this CObject cObject)
+        {
+            if (cObject is COperator)
+            {
+                var cOperator = cObject as COperator;
+                if (cOperator.OpCode.Name == OpCodeName.Tj.ToString() ||
+                    cOperator.OpCode.Name == OpCodeName.TJ.ToString())
+                {
+                    foreach (var cOperand in cOperator.Operands)
+                        foreach (var txt in ExtractText(cOperand))
+                            yield return txt;
+                }
+            }
+            else if (cObject is CSequence)
+            {
+                var cSequence = cObject as CSequence;
+                foreach (var element in cSequence)
+                    foreach (var txt in ExtractText(element))
+                        yield return txt;
+            }
+            else if (cObject is CString)
+            {
+                var cString = cObject as CString;
+                yield return cString.Value;
+            }
+        }
+    }
+    class Program
+    {
+        static async Task<string> TestAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);                                 //异步执行一些任务
+                return "Hello World";                               //异步执行完成标记
+            });
+        }
+        static void Main(string[] args)
+        {
+
+            var str = "dd";
+            str.Split()
+            const string filename = "ShareholderStatement_ERE_L3.pdf";
+            File.Copy(Path.Combine(@"C:\Users\xiaoyan.wu\Documents\visual studio 2015\Projects\ConsoleApplication5\ConsoleApplication5\lib", filename),
+            Path.Combine(Directory.GetCurrentDirectory(), filename), true);
+           
+            
+          
+
+            // Open the file
+            PdfDocument inputDocument = PdfReader.Open(filename, PdfDocumentOpenMode.Import);
+
+            string name = Path.GetFileNameWithoutExtension(filename);
+            for (int idx = 0; idx < inputDocument.PageCount; idx++)
+            {
+                // Create new document
+                PdfDocument outputDocument = new PdfDocument();
+                outputDocument.Version = inputDocument.Version;
+                outputDocument.Info.Title =
+                  String.Format("Page {0} of {1}", idx + 1, inputDocument.Info.Title);
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+
+                var page = inputDocument.Pages[idx];
+                var strContent=page.ExtractText();
+               
+            }
+        }
+    }
+}
